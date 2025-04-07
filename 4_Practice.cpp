7ffcb43564caa335f6f3759731262e181c16d21e
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-08 00:16:34 +0530

    Add files via upload

---
+++ 4_Practice.cpp
@@ -0,0 +1,15 @@
+// Sum Using Variables:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A = 30;
+    int B = 29;
+    int sum = A + B;
+    // cout << "Enter your value:\n";
+    // cin >> A;
+    // cout << "Enter your value:\n";
+    // cin >> B;
+    cout << sum;
+    return 0;
+}
