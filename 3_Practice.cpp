ce1522038e90a5a1abba88b4257e3bf5ab118460
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-08 00:00:24 +0530

    Add files via upload

---
+++ 3_Practice.cpp
@@ -0,0 +1,9 @@
+// Size of Fundamental Data Types
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long long int) << endl;
+}
