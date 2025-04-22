ee03bf857cf73d50ec3b8e2019fb2d7985cdc8af
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-23 00:33:31 +0530

    Add files via upload

---
+++ 31_Practice.cpp
@@ -0,0 +1,22 @@
+// Write a program in C++ to divide two numbers and print them on the screen:
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n\n Divide two numbers and print:\n";
+    cout << "----------------------------------\n";
+
+    int a;
+    int b;
+    int resdiv;
+
+    a = 30;
+    b = 10;
+    resdiv = a / b;
+
+    cout << " The quotient of " << a << " and " << b << " is : " << resdiv << "\n\n";
+
+    return 0;
+}
