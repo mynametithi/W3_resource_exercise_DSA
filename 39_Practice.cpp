122db874c56472edabff4b0bab22910541d033a0
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-28 23:14:03 +0530

    Add files via upload

---
+++ 39_Practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter your first name:";
+    cin >> x;
+    cout << "Enter your last name:";
+    cin >> y;
+    cout << y << " " << x;
+    return 0;
+}
