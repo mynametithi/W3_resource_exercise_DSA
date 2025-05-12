3aadae69275647a560673f29a4b4a07e425a8b1d
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-05-12 21:16:21 +0530

    Add files via upload

---
+++ 50_Practice.cpp
@@ -0,0 +1,17 @@
+// Write a C++ program to replace all the lower-case letters in a given string with the corresponding capital letters:
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string text;
+
+    getline(cin, text);
+
+    transform(text.begin(), text.end(), text.begin(), ::toupper);
+
+    cout << text << endl;
+
+    return 0;
+}
