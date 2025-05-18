32dca23f7f7c554d638b92ed9cbe27296845b671
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-05-18 23:01:24 +0530

    Add files via upload

--- 52_Practice.cpp
+++ 52_Practice.cpp
@@ -1,1 +1,31 @@
-//
+// Write a C++ program to capitalize the first letter of each word in a given string. Words must be separated by only one space.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Capitalize_first_letter(string text)
+{
+
+    for (int x = 0; x < text.length(); x++)
+    {
+
+        if (x == 0 || text[x - 1] == ' ')
+        {
+            text[x] = toupper(text[x]);
+        }
+    }
+
+    return text;
+}
+
+int main()
+{
+
+    cout << Capitalize_first_letter("Write a C++ program");
+
+    cout << "\n"
+         << Capitalize_first_letter("cpp string exercises");
+
+    return 0;
+}
