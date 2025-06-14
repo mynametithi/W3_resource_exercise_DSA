8307dce5bdcc93cb6442a66bdeecfa2ab6678722
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-06-14 23:50:36 +0530

    Add files via upload

---
+++ 6_Practice.cpp
@@ -0,0 +1,27 @@
+// Write a C++ program that checks whether primitive values cross the limit.
+#include <iostream>
+using namespace std;
+int main()
+{
+    char gender = 'F';
+    bool isEmployed = true;
+    unsigned short numOfsons = 2;
+    short yearOfAppt = 2009;
+    unsigned int YearlyPackage = 1500000;
+    double height = 79.48;
+    float gpa = 4.69f;
+    long totalDrawan = 12047235L;
+    long long balance = 995324987LL;
+
+    // outing statement:
+    cout << " The Gender is : " << gender << endl;
+    cout << " Is she married? : " << isEmployed << endl;
+    cout << " Number of sons she has : " << numOfsons << endl;
+    cout << " Year of her appointment : " << yearOfAppt << endl;
+    cout << " Salary for a year : " << YearlyPackage << endl;
+    cout << " Height is : " << height << endl;
+    cout << " GPA is " << gpa << endl;
+    cout << " Salary drawn up to : " << totalDrawan << endl;
+    cout << " Balance till : " << balance << endl;
+    return 0;
+}
