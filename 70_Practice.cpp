c8b0cd32c0d6276280c062dd055e9337bcfa618b
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-06-21 09:28:14 +0530

    Add files via upload

--- 70_Practice.cpp
+++ 70_Practice.cpp
@@ -1,6 +1,12 @@
-//Write a program in C++ to find the first 10 natural numbers:
-#include<iostream>
+// Write a program in C++ to find the first 10 natural numbers:
+#include <iostream>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int i;
+    cout << "Using loop first 10 natural number:" << endl; // loop
+    for (i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
 }
