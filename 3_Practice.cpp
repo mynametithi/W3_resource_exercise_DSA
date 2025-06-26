a030ff63b9f63c20528805fc409cdfd64af502f0
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-06-26 22:55:05 +0530

    Add files via upload

--- 3_Practice.cpp
+++ 3_Practice.cpp
@@ -6,4 +6,10 @@ int main()
     cout << sizeof(long int) << endl;
     cout << sizeof(short int) << endl;
     cout << sizeof(long long int) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(long double) << endl;
+    cout << sizeof(bool) << endl;
 }
